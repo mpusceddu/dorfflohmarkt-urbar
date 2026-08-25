bb4ebcd5cd5afff3436eb22cdb9bb13ff91773ba
Marco Pusceddu <87178473+mpusceddu@users.noreply.github.com>
2026-08-25 16:57:09 +0200

    Leere Datenstruktur für bis zu 82 Flohmarktstände anlegen

---
+++ standdaten.js
@@ -0,0 +1,18 @@
+/*
+ * Hier werden später die bis zu 82 echten Flohmarktstände eingetragen.
+ * Keine Platzhalterstände verwenden.
+ *
+ * Beispiel für EINEN späteren Eintrag:
+ * {
+ *   nummer: 1,
+ *   adresse: "Straße Hausnummer, 56182 Urbar",
+ *   angebot: "Optional: kurze Beschreibung",
+ *   lat: 50.000000,
+ *   lng: 7.000000
+ * }
+ *
+ * nummer und adresse sind Pflichtfelder.
+ * angebot ist optional.
+ * lat und lng werden benötigt, um den Stand als Marker auf der Karte zu zeigen.
+ */
+const STANDDATEN = [];
