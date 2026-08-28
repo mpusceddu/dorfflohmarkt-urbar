1a9e9837c12fbbd2c3d1783b1df164433148f69b
Marco Pusceddu <87178473+mpusceddu@users.noreply.github.com>
2026-08-28 16:26:56 +0200

    86 freigegebene Stände anonymisiert einpflegen

--- standdaten.js
+++ standdaten.js
@@ -1,92 +1,876 @@
 /*
- * Datenmodell:
- * Ein ORT besitzt eine Adresse, Koordinaten, einen Typ und einen oder mehrere Stände.
- *
- * typ:
- * - "privat"    = privates Grundstück mit in der Regel einem Stand
- * - "mehrfach"  = öffentlicher Standort mit einem oder mehreren Ständen
- *
- * Ein Stand benötigt eine eindeutige nummer.
- * angebot und besonderheiten sind optional.
- *
- * Beispiel für einen Kuchenhinweis:
- * {
- *   nummer: 17,
- *   angebot: "Hausrat und Bücher",
- *   besonderheiten: ["Kuchen"]
- * }
- *
- * Keine unbekannten Standdaten ergänzen. Nummern, Angebote und Besonderheiten
- * werden erst nach Freigabe der endgültigen Liste eingetragen.
+ * Öffentliche Standdaten für den Dorfflohmarkt Urbar 2026.
+ * Jede Zeile der freigegebenen Liste entspricht einem Stand.
+ * Namen und interne Hinweise werden hier bewusst nicht veröffentlicht.
+ * Mehrere Stände an derselben Adresse werden zu einem Kartenort gebündelt.
  */
 const ORTSDATEN = [
   {
-    id: "buergerhaus-urbar",
-    name: "Bürgerhaus Urbar",
-    adresse: "Bornstraße 25, 56182 Urbar",
-    typ: "mehrfach",
-    lat: 50.3798707,
-    lng: 7.6249404,
-    staende: [
+    "id": "franziskanerinnenstrasse-5",
+    "adresse": "Franziskanerinnenstraße 5, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3861443,
+    "lng": 7.6255413,
+    "staende": [
       {
-        nummer: 1
+        "nummer": 1
       }
     ]
   },
   {
-    id: "am-rheineck-30",
-    adresse: "Am Rheineck 30, 56182 Urbar",
-    typ: "privat",
-    lat: 50.3780683,
-    lng: 7.6222659,
-    staende: [
+    "id": "remigiusstrasse-11",
+    "adresse": "Remigiusstraße 11, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3857954,
+    "lng": 7.6247458,
+    "staende": [
       {
-        nummer: 2
+        "nummer": 2
+      }
+    ]
+  },
+  {
+    "id": "remigiusstrasse-13a",
+    "adresse": "Remigiusstraße 13a, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3857954,
+    "lng": 7.6247458,
+    "staende": [
+      {
+        "nummer": 3
+      }
+    ]
+  },
+  {
+    "id": "hilda-von-stedman-strasse-1",
+    "adresse": "Hilda-von-Stedman-Straße 1, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3851174,
+    "lng": 7.6247365,
+    "staende": [
+      {
+        "nummer": 4
+      }
+    ]
+  },
+  {
+    "id": "hilda-von-stedman-strasse-12b",
+    "adresse": "Hilda-von-Stedman-Straße 12b, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3851005,
+    "lng": 7.6265149,
+    "staende": [
+      {
+        "nummer": 5
+      }
+    ]
+  },
+  {
+    "id": "franziskanerinnenstrasse-11",
+    "adresse": "Franziskanerinnenstraße 11, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3855023,
+    "lng": 7.6266704,
+    "staende": [
+      {
+        "nummer": 6
+      }
+    ]
+  },
+  {
+    "id": "helfensteinstrasse-1c",
+    "adresse": "Helfensteinstraße 1c, 56182 Urbar",
+    "typ": "mehrfach",
+    "lat": 50.3839794,
+    "lng": 7.6247377,
+    "staende": [
+      {
+        "nummer": 7
+      },
+      {
+        "nummer": 8
+      }
+    ]
+  },
+  {
+    "id": "helfensteinstrasse-30",
+    "adresse": "Helfensteinstraße 30, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3839794,
+    "lng": 7.6247377,
+    "staende": [
+      {
+        "nummer": 9
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-113",
+    "adresse": "Hauptstraße 113, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.383541,
+    "lng": 7.6244122,
+    "staende": [
+      {
+        "nummer": 10
+      }
+    ]
+  },
+  {
+    "id": "arenberger-strasse-4",
+    "adresse": "Arenberger Straße 4, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3831347,
+    "lng": 7.6245818,
+    "staende": [
+      {
+        "nummer": 11
+      }
+    ]
+  },
+  {
+    "id": "arenberger-strasse-6",
+    "adresse": "Arenberger Straße 6, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3829975,
+    "lng": 7.6245432,
+    "staende": [
+      {
+        "nummer": 12
+      }
+    ]
+  },
+  {
+    "id": "arenberger-strasse-12",
+    "adresse": "Arenberger Straße 12, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3828525,
+    "lng": 7.6247929,
+    "staende": [
+      {
+        "nummer": 13
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-111",
+    "adresse": "Hauptstraße 111, 56182 Urbar",
+    "typ": "mehrfach",
+    "lat": 50.383083,
+    "lng": 7.6240003,
+    "staende": [
+      {
+        "nummer": 14
+      },
+      {
+        "nummer": 15
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-105",
+    "adresse": "Hauptstraße 105, 56182 Urbar",
+    "typ": "mehrfach",
+    "lat": 50.3828181,
+    "lng": 7.6234669,
+    "staende": [
+      {
+        "nummer": 16
+      },
+      {
+        "nummer": 17
+      },
+      {
+        "nummer": 18
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-103",
+    "adresse": "Hauptstraße 103, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.382702,
+    "lng": 7.6233993,
+    "staende": [
+      {
+        "nummer": 19
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-102",
+    "adresse": "Hauptstraße 102, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3825992,
+    "lng": 7.6229251,
+    "staende": [
+      {
+        "nummer": 20
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-100",
+    "adresse": "Hauptstraße 100, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3823685,
+    "lng": 7.6230987,
+    "staende": [
+      {
+        "nummer": 21
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-89",
+    "adresse": "Hauptstraße 89, 56182 Urbar",
+    "typ": "mehrfach",
+    "lat": 50.3820755,
+    "lng": 7.6231637,
+    "staende": [
+      {
+        "nummer": 22
+      },
+      {
+        "nummer": 23
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-95",
+    "adresse": "Hauptstraße 95, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3822785,
+    "lng": 7.6235591,
+    "staende": [
+      {
+        "nummer": 24
+      }
+    ]
+  },
+  {
+    "id": "in-den-bungerten-20",
+    "adresse": "In den Büngerten 20, 56182 Urbar",
+    "typ": "mehrfach",
+    "lat": 50.3822695,
+    "lng": 7.6239695,
+    "staende": [
+      {
+        "nummer": 25
+      },
+      {
+        "nummer": 26
+      }
+    ]
+  },
+  {
+    "id": "in-den-bungerten-28",
+    "adresse": "In den Büngerten 28, 56182 Urbar",
+    "typ": "mehrfach",
+    "lat": 50.3820811,
+    "lng": 7.6242658,
+    "staende": [
+      {
+        "nummer": 27
+      },
+      {
+        "nummer": 28
+      },
+      {
+        "nummer": 29
+      }
+    ]
+  },
+  {
+    "id": "arenberger-strasse-24",
+    "adresse": "Arenberger Straße 24, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3822968,
+    "lng": 7.6251249,
+    "staende": [
+      {
+        "nummer": 30
+      }
+    ]
+  },
+  {
+    "id": "friedrich-ebert-strasse-14",
+    "adresse": "Friedrich-Ebert-Straße 14, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3826151,
+    "lng": 7.6260494,
+    "staende": [
+      {
+        "nummer": 31
+      }
+    ]
+  },
+  {
+    "id": "friedrich-ebert-strasse-28",
+    "adresse": "Friedrich-Ebert-Straße 28, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3815827,
+    "lng": 7.626811,
+    "staende": [
+      {
+        "nummer": 32
+      }
+    ]
+  },
+  {
+    "id": "friedrich-ebert-strasse-29",
+    "adresse": "Friedrich-Ebert-Straße 29, 56182 Urbar",
+    "typ": "mehrfach",
+    "lat": 50.3811947,
+    "lng": 7.6267393,
+    "staende": [
+      {
+        "nummer": 33
+      },
+      {
+        "nummer": 34
+      }
+    ]
+  },
+  {
+    "id": "arenberger-strasse-46",
+    "adresse": "Arenberger Straße 46, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3810896,
+    "lng": 7.6257201,
+    "staende": [
+      {
+        "nummer": 35
+      }
+    ]
+  },
+  {
+    "id": "arenberger-strasse-25a",
+    "adresse": "Arenberger Straße 25a, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3812889,
+    "lng": 7.6253386,
+    "staende": [
+      {
+        "nummer": 36
+      }
+    ]
+  },
+  {
+    "id": "in-den-bungerten-9",
+    "adresse": "In den Büngerten 9, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.38145,
+    "lng": 7.6247846,
+    "staende": [
+      {
+        "nummer": 37
+      }
+    ]
+  },
+  {
+    "id": "in-den-bungerten-8",
+    "name": "Katholische Kita St. Peter und Paul",
+    "adresse": "In den Büngerten 8, 56182 Urbar",
+    "typ": "mehrfach",
+    "lat": 50.3814375,
+    "lng": 7.6239305,
+    "staende": [
+      {
+        "nummer": 38
+      },
+      {
+        "nummer": 39
+      },
+      {
+        "nummer": 40
+      },
+      {
+        "nummer": 41
+      },
+      {
+        "nummer": 42
+      },
+      {
+        "nummer": 43
+      },
+      {
+        "nummer": 44
+      },
+      {
+        "nummer": 45
+      },
+      {
+        "nummer": 46,
+        "angebot": "Foodstand Kaffee, Würstchen & Kuchen",
+        "besonderheiten": [
+          "Kuchen"
+        ]
+      },
+      {
+        "nummer": 47
+      },
+      {
+        "nummer": 48
+      },
+      {
+        "nummer": 49
+      },
+      {
+        "nummer": 50
+      }
+    ]
+  },
+  {
+    "id": "schulstrasse-6",
+    "adresse": "Schulstraße 6, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3809006,
+    "lng": 7.6240174,
+    "staende": [
+      {
+        "nummer": 51
+      }
+    ]
+  },
+  {
+    "id": "bornstrasse-24",
+    "adresse": "Bornstraße 24, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3807131,
+    "lng": 7.625472,
+    "staende": [
+      {
+        "nummer": 52
+      }
+    ]
+  },
+  {
+    "id": "arenberger-strasse-50",
+    "adresse": "Arenberger Straße 50, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3804872,
+    "lng": 7.6262,
+    "staende": [
+      {
+        "nummer": 53
+      }
+    ]
+  },
+  {
+    "id": "arenberger-strasse-33",
+    "adresse": "Arenberger Straße 33, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.380121,
+    "lng": 7.6260031,
+    "staende": [
+      {
+        "nummer": 54
+      }
+    ]
+  },
+  {
+    "id": "arenberger-strasse-57",
+    "adresse": "Arenberger Straße 57, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3796974,
+    "lng": 7.6268805,
+    "staende": [
+      {
+        "nummer": 55
+      }
+    ]
+  },
+  {
+    "id": "friedrich-ebert-strasse-39",
+    "adresse": "Friedrich-Ebert-Straße 39, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3804506,
+    "lng": 7.6271277,
+    "staende": [
+      {
+        "nummer": 56
+      }
+    ]
+  },
+  {
+    "id": "am-muhlenberg-7",
+    "adresse": "Am Mühlenberg 7, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3793677,
+    "lng": 7.6289658,
+    "staende": [
+      {
+        "nummer": 57
+      }
+    ]
+  },
+  {
+    "id": "am-muhlenberg-9",
+    "adresse": "Am Mühlenberg 9, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.379253,
+    "lng": 7.6291117,
+    "staende": [
+      {
+        "nummer": 58
+      }
+    ]
+  },
+  {
+    "id": "am-muhlenberg-10",
+    "adresse": "Am Mühlenberg 10, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3791031,
+    "lng": 7.6287645,
+    "staende": [
+      {
+        "nummer": 59
+      }
+    ]
+  },
+  {
+    "id": "am-muhlenberg-16",
+    "adresse": "Am Mühlenberg 16, 56182 Urbar",
+    "typ": "mehrfach",
+    "lat": 50.3788494,
+    "lng": 7.6291682,
+    "staende": [
+      {
+        "nummer": 60
+      },
+      {
+        "nummer": 61
+      }
+    ]
+  },
+  {
+    "id": "am-muhlenberg-15",
+    "adresse": "Am Mühlenberg 15, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3788474,
+    "lng": 7.6297509,
+    "staende": [
+      {
+        "nummer": 62
+      }
+    ]
+  },
+  {
+    "id": "am-hellengraben-30",
+    "adresse": "Am Hellengraben 30, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3770313,
+    "lng": 7.6286052,
+    "staende": [
+      {
+        "nummer": 63
+      }
+    ]
+  },
+  {
+    "id": "am-schutzenplatz-9",
+    "adresse": "Am Schützenplatz 9, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3767808,
+    "lng": 7.6255215,
+    "staende": [
+      {
+        "nummer": 64
+      }
+    ]
+  },
+  {
+    "id": "am-schutzenplatz-16",
+    "adresse": "Am Schützenplatz 16, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3762207,
+    "lng": 7.6253238,
+    "staende": [
+      {
+        "nummer": 65
+      }
+    ]
+  },
+  {
+    "id": "im-monzental-27",
+    "adresse": "Im Monzental 27, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3760537,
+    "lng": 7.623449,
+    "staende": [
+      {
+        "nummer": 66
+      }
+    ]
+  },
+  {
+    "id": "rheinhohe-8",
+    "adresse": "Rheinhöhe 8, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3770626,
+    "lng": 7.6215406,
+    "staende": [
+      {
+        "nummer": 67
+      }
+    ]
+  },
+  {
+    "id": "am-rheineck-30",
+    "adresse": "Am Rheineck 30, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3780683,
+    "lng": 7.6222659,
+    "staende": [
+      {
+        "nummer": 68
+      }
+    ]
+  },
+  {
+    "id": "hammerstein-16",
+    "adresse": "Hammerstein 16, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.378838,
+    "lng": 7.6221059,
+    "staende": [
+      {
+        "nummer": 69
+      }
+    ]
+  },
+  {
+    "id": "hammerstein-14",
+    "adresse": "Hammerstein 14, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3789363,
+    "lng": 7.6221317,
+    "staende": [
+      {
+        "nummer": 70
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-51",
+    "adresse": "Hauptstraße 51, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.37964,
+    "lng": 7.6213878,
+    "staende": [
+      {
+        "nummer": 71
+      }
+    ]
+  },
+  {
+    "id": "bornstrasse-27",
+    "name": "Kita Unter dem Regenbogen",
+    "adresse": "Bornstraße 27, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3799433,
+    "lng": 7.6223842,
+    "staende": [
+      {
+        "nummer": 72
+      }
+    ]
+  },
+  {
+    "id": "gartenstrasse-13",
+    "adresse": "Gartenstraße 13, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3803673,
+    "lng": 7.6227502,
+    "staende": [
+      {
+        "nummer": 73
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-65",
+    "adresse": "Hauptstraße 65, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3806429,
+    "lng": 7.6224411,
+    "staende": [
+      {
+        "nummer": 74
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-84",
+    "adresse": "Hauptstraße 84, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3815388,
+    "lng": 7.6226633,
+    "staende": [
+      {
+        "nummer": 75
+      }
+    ]
+  },
+  {
+    "id": "kirchstrasse-4",
+    "adresse": "Kirchstraße 4, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.379382,
+    "lng": 7.6235221,
+    "staende": [
+      {
+        "nummer": 76
+      }
+    ]
+  },
+  {
+    "id": "kirchstrasse-8",
+    "adresse": "Kirchstraße 8, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3788178,
+    "lng": 7.6236013,
+    "staende": [
+      {
+        "nummer": 77
+      }
+    ]
+  },
+  {
+    "id": "kirchstrasse-10",
+    "adresse": "Kirchstraße 10, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3786936,
+    "lng": 7.6234813,
+    "staende": [
+      {
+        "nummer": 78
+      }
+    ]
+  },
+  {
+    "id": "eichendorffweg-12",
+    "adresse": "Eichendorffweg 12, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.377989,
+    "lng": 7.6243639,
+    "staende": [
+      {
+        "nummer": 79
+      }
+    ]
+  },
+  {
+    "id": "eichendorffweg-8",
+    "adresse": "Eichendorffweg 8, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3783163,
+    "lng": 7.6248218,
+    "staende": [
+      {
+        "nummer": 80
+      }
+    ]
+  },
+  {
+    "id": "eichendorffweg-4",
+    "adresse": "Eichendorffweg 4, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3785837,
+    "lng": 7.625211,
+    "staende": [
+      {
+        "nummer": 81
+      }
+    ]
+  },
+  {
+    "id": "am-sportplatz-4",
+    "adresse": "Am Sportplatz 4, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3790305,
+    "lng": 7.6250132,
+    "staende": [
+      {
+        "nummer": 82
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-33",
+    "adresse": "Hauptstraße 33, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.3786829,
+    "lng": 7.6205221,
+    "staende": [
+      {
+        "nummer": 83
+      }
+    ]
+  },
+  {
+    "id": "hauptstrasse-22",
+    "adresse": "Hauptstraße 22, 56182 Urbar",
+    "typ": "mehrfach",
+    "lat": 50.3784764,
+    "lng": 7.6192457,
+    "staende": [
+      {
+        "nummer": 84
+      },
+      {
+        "nummer": 85
+      }
+    ]
+  },
+  {
+    "id": "am-kammradchen-2",
+    "adresse": "Am Kammrädchen 2, 56182 Urbar",
+    "typ": "privat",
+    "lat": 50.382582,
+    "lng": 7.6278797,
+    "staende": [
+      {
+        "nummer": 86
       }
     ]
   }
 ];
 
-/*
- * Orientierungspunkte werden unabhängig von den Flohmarktständen gepflegt.
- * Sie besitzen keine Standnummer und werden deshalb nicht zu den 82 Ständen
- * hinzugerechnet.
- *
- * typ:
- * - "verpflegung" = Foodstation oder anderer Versorgungspunkt
- * - "parkplatz"   = ausgewiesene Parkmöglichkeit
- */
+/* Orientierungspunkte ohne Standnummer. */
 const SERVICEPUNKTE = [
   {
-    id: "foodstation-kita-peter-und-paul",
-    name: "Foodstation",
-    adresse: "Katholische Kita St. Peter und Paul, In den Büngerten 8, 56182 Urbar",
-    typ: "verpflegung",
-    lat: 50.3814834,
-    lng: 7.6244177
+    "id": "foodstation-kita-peter-und-paul",
+    "name": "Foodstation",
+    "adresse": "Katholische Kita St. Peter und Paul, In den Büngerten 8, 56182 Urbar",
+    "typ": "verpflegung",
+    "lat": 50.3814834,
+    "lng": 7.6244177
   },
   {
-    id: "parkplatz-besselicher-feld",
-    name: "Parkplatz Besselicher Feld",
-    adresse: "Beginenstraße 2, 56182 Urbar",
-    typ: "parkplatz",
-    lat: 50.3849565,
-    lng: 7.6237414
+    "id": "parkplatz-besselicher-feld",
+    "name": "Parkplatz Besselicher Feld",
+    "adresse": "Beginenstraße 2, 56182 Urbar",
+    "typ": "parkplatz",
+    "lat": 50.3849565,
+    "lng": 7.6237414
   },
   {
-    id: "parkplatz-friedhof",
-    name: "Parkplatz Friedhof",
-    adresse: "Arenberger Straße / In der Hohl, 56182 Urbar",
-    typ: "parkplatz",
-    lat: 50.3784206,
-    lng: 7.6270259
+    "id": "parkplatz-friedhof",
+    "name": "Parkplatz Friedhof",
+    "adresse": "Arenberger Straße / In der Hohl, 56182 Urbar",
+    "typ": "parkplatz",
+    "lat": 50.3784206,
+    "lng": 7.6270259
   },
   {
-    id: "parkplatz-alter-kirchplatz",
-    name: "Parkplatz Alter Kirchplatz",
-    adresse: "Alter Kirchplatz / Hauptstraße, 56182 Urbar",
-    typ: "parkplatz",
-    lat: 50.3810522,
-    lng: 7.6227592
+    "id": "parkplatz-alter-kirchplatz",
+    "name": "Parkplatz Alter Kirchplatz",
+    "adresse": "Alter Kirchplatz / Hauptstraße, 56182 Urbar",
+    "typ": "parkplatz",
+    "lat": 50.3810522,
+    "lng": 7.6227592
   }
 ];
+
